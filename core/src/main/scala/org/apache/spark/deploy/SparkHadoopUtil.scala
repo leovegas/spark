e0e74475663c04c0a3bff7e9e0b56f3474c40943
Leonid Timofeev <leonid.timofeev@kambi.com>
2024-05-15 21:38:04 +0200

    SPARK-47008: Added hasPathCapability check to avoid throwing a FileNotFoundException when listing file statuses, enhancing support for S3 Express One Zone Storage.

--- core/src/main/scala/org/apache/spark/deploy/SparkHadoopUtil.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkHadoopUtil.scala
@@ -27,6 +27,7 @@ import scala.collection.mutable
 import scala.collection.mutable.HashMap
 import scala.jdk.CollectionConverters._
 import scala.language.existentials
+import scala.util.{Failure, Success, Try}
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs._
@@ -217,10 +218,25 @@ private[spark] class SparkHadoopUtil extends Logging {
    * given path points to a file, return a single-element collection containing [[FileStatus]] of
    * that file.
    */
+  @throws[FileNotFoundException]("when the fs.listStatus throws FileNotFoundException and " +
+    "the value of fs.hasPathCapability is FALSE")
   def listLeafStatuses(fs: FileSystem, baseStatus: FileStatus): Seq[FileStatus] = {
     def recurse(status: FileStatus): Seq[FileStatus] = {
-      val (directories, leaves) = fs.listStatus(status.getPath).partition(_.isDirectory)
-      (leaves ++ directories.flatMap(f => listLeafStatuses(fs, f))).toImmutableArraySeq
+      val fsHasPathCapability =
+        fs.hasPathCapability(status.getPath, SparkHadoopUtil.DIRECTORY_LISTING_INCONSISTENT)
+      val statusResult = Try {
+        fs.listStatus(status.getPath)
+      }
+      statusResult match {
+        case Failure(e) =>
+          if (e.isInstanceOf[FileNotFoundException] && fsHasPathCapability) {
+            Seq.empty[FileStatus]
+          }
+          else throw e
+        case Success(sr) =>
+          val (directories, leaves) = sr.partition(_.isDirectory)
+          (leaves ++ directories.flatMap(f => listLeafStatuses(fs, f))).toImmutableArraySeq
+      }
     }
 
     if (baseStatus.isDirectory) recurse(baseStatus) else Seq(baseStatus)
@@ -354,6 +370,9 @@ private[spark] object SparkHadoopUtil extends Logging {
    * Hadoop FileSystem API of interest (only available in 2.5), so we should do this sparingly.
    */
   private[spark] val UPDATE_INPUT_METRICS_INTERVAL_RECORDS = 1000
+
+
+  private[spark] val DIRECTORY_LISTING_INCONSISTENT = "fs.capability.directory.listing.inconsistent"
 
   /**
    * Name of the file containing the gateway's Hadoop configuration, to be overlayed on top of the
